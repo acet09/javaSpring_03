7e10fa3f258e3e425cabdf35de60c2b7df4e9fbb
Cactus <ysjungman@gmail.com>
2023-07-01 17:10:35 +0900

    44_component-more

--- chap_03/src/main/java/com/in28minutes/learnspringframework/examples/c1/RealWorldSpringContextLauncherApplication.java
+++ chap_03/src/main/java/com/in28minutes/learnspringframework/examples/c1/RealWorldSpringContextLauncherApplication.java
@@ -13,9 +13,10 @@ public class RealWorldSpringContextLauncherApplication {
 	public static void main(String[] args) {
 
 		try (var context = new AnnotationConfigApplicationContext(RealWorldSpringContextLauncherApplication.class)) {
-			Arrays.stream(context.getBeanDefinitionNames()).forEach(System.out::println);
+			Arrays.stream(context.getBeanDefinitionNames())
+				.forEach(System.out::println);
 
-			System.out.println(context.getBean(BusinessCalculationService.class).findMax());
+//			System.out.println(context.getBean(BusinessCalculationService.class).findMax());
 
 		}
 	}
